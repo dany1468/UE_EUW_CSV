ea49829ebd346db3787f41dd13d97807b7bd2d32
dany1468 <dany1468@gmail.com>
2022-03-19 08:20:25 +0900

    Editor Utility Widget から Python Script を呼び出すようにした

---
+++ Content/Python/load.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fffdf3d64f263dc9405f19034ec3b348f7206927acd0b300b8b033ee4e8cb4ce
+size 35
---
+++ Content/Python/save.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:24592c319e95d25cc68a38d51ea4ccd2b911f2e95f1c1bfb799670d0c1e4c2ab
+size 35
